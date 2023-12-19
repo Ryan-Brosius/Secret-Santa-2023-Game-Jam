38dd2e22b660fa6295a3b4ef93a8f94492867667
chr47 <chr47@drexel.edu>
2023-12-18 19:33:49 -0500

    more health stuff
    health bar for bosses
    
    screen shake on damage
    
    screen freeze on damage

--- Tohouh Interstellar/Assets/Scripts/PlayerHealth.cs
+++ Tohouh Interstellar/Assets/Scripts/PlayerHealth.cs
@@ -8,7 +8,9 @@ public class PlayerHealth : MonoBehaviour
     public float currentHealth;
     public float colorTickTime = 0.1f;
     public bool canTakeDamage = true;
-    public float iFrameTime = 0.1f;
+    public float invincibleFrameTime = 0.5f;
+    public float freezeTime = 0.2f;
+    public CameraShake shake;
 
 
     // Start is called before the first frame update
@@ -26,25 +28,41 @@ public class PlayerHealth : MonoBehaviour
             {
                 Destroy(gameObject);
             }
-            StartCoroutine(damageTick());
-            StartCoroutine(iFrames());
+
+            StartCoroutine(takeDamage());
+            StartCoroutine(invincibleFrames());
         }
+    }
+
+    IEnumerator takeDamage()
+    {
+        Time.timeScale = 0;
+
+        StartCoroutine(damageTick());
+
+        StartCoroutine(shake.Shake(.15f, .03f));
+
+        yield return new WaitForSecondsRealtime(freezeTime);
+
+        StartCoroutine(damageTick());
+
+        Time.timeScale = 1;
     }
 
     IEnumerator damageTick()
     {
         gameObject.GetComponent<SpriteRenderer>().color = new Color(255, 0, 0);
 
-        yield return new WaitForSeconds(colorTickTime);
+        yield return new WaitForSecondsRealtime(colorTickTime);
 
         gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1);
     }
 
-    IEnumerator iFrames()
+    IEnumerator invincibleFrames()
     {
         canTakeDamage = false;
 
-        yield return new WaitForSeconds(iFrameTime);
+        yield return new WaitForSeconds(invincibleFrameTime);
 
         canTakeDamage = true;
 
